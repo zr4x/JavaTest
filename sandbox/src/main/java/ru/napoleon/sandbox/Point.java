9f020d3e0e150c2e4bb33b03127784e632c3ba3b
Nikolay Kozlov <skorpiod@gmail.com>
2017-01-22 21:20:16 +0500

    ready for merge in master

--- sandbox/src/main/java/ru/napoleon/sandbox/Point.java
+++ sandbox/src/main/java/ru/napoleon/sandbox/Point.java
@@ -7,26 +7,17 @@ package ru.napoleon.sandbox;
 
 public class Point {
 
-    double x1;
-    double x2;
-    double y1;
-    double y2;
+    public double x;
+    public double y;
 
 
-    public static void main(String[] args) {
-        Point point = new Point(1,2,3,7);
-        System.out.println("Расстояние до точки  = " + point.distance());
-
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public Point(double x1, double y1, double x2, double y2) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.y1 = y1;
-        this.y2 = y2;
+    public double destination(Point p1){
+        return Math.sqrt((Math.pow((this.x - p1.x), 2)) + (Math.pow((this.y-p1.y), 2)));
     }
 
-    public double distance() {
-        return Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2));
-    } 
 }
